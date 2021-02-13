a17cf97cff524dbd7d887b7c92daa307ad99dfdd
Tanmay <meet10may@gmail.com>
2021-02-13 02:10:34 +0000

    organize code

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
+from PIL import Image
 import scipy.io as sio
 import numpy as np
 import os, pickle,glob
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from keras.models import load_model
-from keras.callbacks import LearningRateScheduler, ModelCheckpoint, EarlyStopping
+# from tensorflow.keras.callbacks import EarlyStopping # specific for tf >2
+from tensorflow.keras.callbacks import LearningRateScheduler, ModelCheckpoint, EarlyStopping
 from scipy.stats import zscore
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
@@ -14,7 +16,10 @@ import nibabel as nib
 from nilearn.datasets import load_mni152_template
 from nilearn.image import resample_to_img,load_img,index_img,resample_img,concat_imgs,load_img,resample_to_img,threshold_img,mean_img
 from nilearn import plotting
-
+from scipy.stats import zscore,norm
+from sklearn.datasets import fetch_openml
+mnist_dataset = fetch_openml('mnist_784')
+import build_model
 
 # import sys
 # sys.path.append('/home/ubuntu/hacking/projects/deep-mediation/dec-2020/keras-vis-master')
@@ -42,78 +47,103 @@ def plot_train_loss(history,name,save=True):
     if save:
         plt.savefig(name)
         
-# def create_dataset(data_path):
-#     print("Test")
-#     data_info = load_pickle(os.path.join(data_path,'studyinfo.pickle'))
-#     studies_to_include = load_pickle(os.path.join(data_path,'studyinfo.pickle'))['study'][1:8]
-#     test_study = load_pickle(os.path.join(data_path,'studyinfo.pickle'))['study'][-2]
-#     print("These are the studies to use %s"%studies_to_include)
-#     print("This is the test study %s "%test_study)
-#     data_to_include = [os.path.join(data_path,i+'_data') for i in studies_to_include]
+def extract_image(num,mnist_dataset,resize,new_shape,algo='svr'):
+    """
+    Used for data simulation.Returns the image
+    num: string. The string of number and a digit is replaced with an actual image.
+    """
 
-#     imgs = []
-#     for i in data_to_include:
-#         print(i)
-#         if 'ILCP' in i:
-#             data = [os.path.join(data_path,'ILCP'+'_data',s+'.nii') for s in data_info.subjects[5]]
-#         elif 'EXP' in i:
-#             data = [os.path.join(data_path,'EXP'+'_data',s+'.nii') for s in data_info.subjects[6]]
-#         elif 'BMRK3' in i:
-#             data = [os.path.join(data_path,'BMRK3'+'_data','bmrk3_st_'+s+'.nii') for s in data_info.subjects[2]]
-#         else:
-#             data = glob.glob(os.path.join(i,'*.nii')) 
-#             data.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
-#         imgs = imgs+data
+    target = mnist_dataset.target
+    mnist_image = mnist_dataset.data
+    img_shape = (int(np.sqrt(mnist_image.shape[-1])) , int(np.sqrt(mnist_image.shape[-1])))
+    image= []
+    for j in range(len(num)):
+        raw_img = mnist_image[np.argwhere(target == num[j])[0]][0]
+        raw_img = raw_img.reshape(img_shape)
+        new_img = Image.fromarray(raw_img)
+        img = new_img.resize(size=(new_shape[0],new_shape[1]))
+        img = np.asarray(img)
+        if j==0:
+            output = img
+        else:
+            output = concat_mnist_images(output, img)
+        if algo == 'svr':
+            output_ = output.flatten()
+#         if not cnn:
+#             output_ = output.flatten()
+    if algo == 'svr':
+        output = output_
+    image.append(output)
+    return np.concatenate(image) 
 
-#     test_data = [os.path.join(data_path,'BMRK5_data',s.replace('sub','stim_bmrk5_S')+'*.nii') for s in sum(data_info.subjects[-2], [])]
-#     test_data = [glob.glob(i) for i in test_data]
-#     test_data = sum(test_data,[])
-#     dataset= {}
-#     NSF = {}
-#     BMRK3 = {}
-#     BMRK4 = {}
-#     IE = {}
-#     EXP = {}
-#     ILCP = {}
-#     SCEBL = {}
-#     BMRK5 = {}
+def simulate_dataset(num_subs, resize,new_shape,visualize,algo='svr',alpha=0):
+    """
+    Simulate the dataset.
+    num_subs :int; number of subjects;
+    resize: bool; if you want to resize the images;
+    new_shape: list of int; new shape of the images
+    """
 
-#     for i in range(0,26):
-#         NSF[imgs[i]] = [data_info.rate[1][i],data_info.temp[1][i]]
+    #Simulate X with fixed
+    X = np.random.normal(0,1,num_subs)
+    a0,e0 = -0.1,np.random.normal(0,1,num_subs)
+    m = a0 + alpha*X + e0
+    z = norm.cdf(m)
 
-#     for i in range(0,33):
-#         BMRK3[imgs[26+i]] = [data_info.rate[2][i],data_info.temp[2][i]]
+    #Simulate images m
+    M = []
+    floating_pt = [str(i)[2:6] for i in z]
+    for num in floating_pt:
+        image = extract_image(num,mnist_dataset,resize,new_shape,algo)
+        M.append(image)
+    M = np.array(M) #multi-dimensional
+    if algo != 'svr':
+        M = M[:,:,:,np.newaxis] # for keras to add an extra dimension
+    #simulate Y ratings
+
+    beta,gamma,b0 = 4,5,6 #4,5,6
+    error = np.random.normal(0,1,num_subs)
+    Y = b0 + gamma*X + beta*m + error 
     
-#     for i in range(0,28):
-#         BMRK4[imgs[59+i]] = [data_info.rate[3][i],data_info.temp[3][i]]
+    df = pd.DataFrame()
+    df['X'] = X
+    df['Y'] = Y
+    df['m'] = m
     
-#     for i in range(0,50):
-#         IE[imgs[87+i]] = [data_info.rate[4][i],data_info.temp[4][i]]
-    
-#     for i in range(0,29):
-#         ILCP[imgs[137+i]] = [data_info.rate[5][i],data_info.temp[5][i]]
-    
-#     for i in range(0,17):
-#         EXP[imgs[166+i]] = [data_info.rate[6][i],data_info.temp[6][i]]
+    #Create train and test dataset
+    X_train, X_test, y_train, y_test = create_train_data(df,M,testing_dataset_size=0.5)
+    if visualize:
+        idx = 0
+        visualize_dataset(M,z,idx,new_shape)
 
-#     for i in range(0,26):
-#         SCEBL[imgs[183+i]] = [data_info.rate[7][i],data_info.temp[7][i]]
+    return X_train, X_test, y_train, y_test
 
-#     for i in range(0,75):
-#         BMRK5[test_data[i]] = [data_info.rate[8][i],data_info.temp[8][i]]
- 
-#     dataset = {**NSF, **BMRK3,**BMRK4, **IE,**ILCP, **EXP,**SCEBL,**BMRK5}
+def visualize_dataset(M,z,idx,shape):
+    """
+    M: multi-dimensional mediator image
+    z:
+    idx: the index of image to visualize
+    shape: tuple of mediator shape
+    """
+    import matplotlib.pyplot as plt
+    x,y = shape[0],shape[1]*4
+    plt.imshow(M[idx].reshape(x,y))
+    plt.title("The z value for this image is %s"%str(z[idx])[2:6])
+    plt.show()
 
+def create_train_data(X,y,testing_dataset_size=0.5):
+    training_dataset_size = 1-testing_dataset_size
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=testing_dataset_size,shuffle=True,random_state=42)
+    print("Dataset is randomly split into %s%% test dataset and %s%% train dataset"%(testing_dataset_size*100,training_dataset_size*100))
+    print("Total number of subjects in training dataset: %s"%len(X_train))
+    print("Total number of subjects in testing dataset: %s"%len(X_test))
+    return X_train, X_test, y_train, y_test
 
-#     train_subjs, val_subjs = train_test_split(imgs,test_size = 0.15,shuffle=True,random_state=42)
-#     print("Number of training subjects: %s" %len(train_subjs))
-
-#     dataset['train_subjs'] = train_subjs
-#     dataset['val_subjs'] = val_subjs
-#     dataset['study'] = data_info['study'][1:9]
-#     dataset['N'] = data_info['N'][1:9]
-
-#     return dataset
+def tune_params(est,parameters,train_df,train_y,cross_val=5,njobs=-1,verbose=2):
+    scorer = make_scorer(mean_squared_error, greater_is_better=False)
+    est_grid = GridSearchCV(est, parameters,cv=cross_val,n_jobs=njobs,verbose=verbose,scoring=scorer)
+    est_grid.fit(train_df, train_y)
+    return est_grid.best_estimator_
 
 def get_rate_temp_img(dataset,subjs='train_subjs'):
     rate = [dataset[i][0] for i in list(dataset[subjs])]
@@ -127,12 +157,26 @@ def get_rate_temp_img(dataset,subjs='train_subjs'):
     flat_rate_zs = np.array([item for sublist in rate_zs for item in sublist])
     return rate,np.concatenate(temp),imgs,flat_rate,flat_rate_zs
 
+# def create_empty_df(num_runs,num_iters):
+#     # Creates an empty dataFrame
+#     a = np.empty((num_runs,num_iters))
+#     a[:] = np.nan
+#     dataFrame = None
+#     parameters = ['alpha', 'beta','theta','gamma','delta']
+#     for params in parameters:
+#         iter = ['iter_'+str(i) for i in range(num_iters)]
+#         pdindex = pd.MultiIndex.from_product([[params], iter],
+#                                              names=['parameters', 'runs']) 
+#         frame = pd.DataFrame(a, columns = pdindex,index = range(0,num_runs))
+#         dataFrame = pd.concat([dataFrame,frame],axis=1)
+#     return dataFrame
+
 def create_empty_df(num_runs,num_iters):
     # Creates an empty dataFrame
     a = np.empty((num_runs,num_iters))
     a[:] = np.nan
     dataFrame = None
-    parameters = ['alpha', 'beta','theta','gamma','delta']
+    parameters = ['alpha0', 'beta0','alpha','beta','gamma']
     for params in parameters:
         iter = ['iter_'+str(i) for i in range(num_iters)]
         pdindex = pd.MultiIndex.from_product([[params], iter],
@@ -141,27 +185,24 @@ def create_empty_df(num_runs,num_iters):
         dataFrame = pd.concat([dataFrame,frame],axis=1)
     return dataFrame
 
-def simulate_mediation(df_train,df_val,df_test,X_train,X_val,X_test,params_df,model,n_runs,batchSize,nEpochs,iterations,pat,
-                       output_path=None,use_model=None):
+def simulate_mediation(df_train,df_test,X_train,X_test,train_params_df,test_params_df,
+                       model,suffix,n_runs,batchSize,nEpochs,iterations,pat,output_path=None,
+                       use_model=None,algo='deep'):
     """
     """
     
     # Initialize z with some phi
-    
     if use_model!=None:
         model = load_model(use_model)
-        start = int(Path(use_model).stem.split('-')[-1]) #int(Path(use_model).stem[-1])
+        start = int(Path(use_model).stem.split('-')[-1]) 
         print("Using already saved model iteration %s..."%start)
         z_train = model.predict(X_train) 
-#         z_val = model.predict(X_val)
     else:
         print("Using intial random model parameters")
         start = 0
         z_train = model.predict(X_train) 
-#         z_val = model.predict(X_val)
     try:
         z_train = np.concatenate(z_train)
-#         z_val = np.concatenate(z_val)
     except:
         pass
   
@@ -170,77 +211,67 @@ def simulate_mediation(df_train,df_val,df_test,X_train,X_val,X_test,params_df,mo
         # Check for directionality
         if np.corrcoef(z_train,df_train.Y)[0,1] < 0:
               z_train = z_train*(-1)
-#         if np.corrcoef(z_val,df_val.Y)[0,1] < 0:
-#               z_val = z_val*(-1)
         # Check for scaling issue
         z_train = zscore(z_train)
-#         z_val = zscore(z_val)
         
         lm_train = smf.ols(formula='z_train ~ X', data=df_train).fit()
         alpha0_train = lm_train.params.loc['Intercept']
         alph_train = lm_train.params.loc['X']
         
-#         lm_val = smf.ols(formula='z_val ~ X', data=df_val).fit()
-#         alpha0_val = lm_val.params.loc['Intercept']
-#         alph_val = lm_val.params.loc['X']
-
         lm_train = smf.ols(formula='Y ~ z_train + X', data=df_train).fit()
         beta0_train = lm_train.params.Intercept
         bet_train = lm_train.params.z_train
         gam_train = lm_train.params.X
         resid_std_train = np.std(lm_train.resid)
 
-#         lm_val = smf.ols(formula='Y ~ z_val + X', data=df_val).fit()
-#         beta0_val = lm_val.params.Intercept
-#         bet_val = lm_val.params.z_val
-#         gam_val = lm_val.params.X
-#         resid_std_val = np.std(lm_val.resid)
-
         e_train = df_train.Y - beta0_train - (df_train.X*gam_train)
         h_train = alpha0_train + df_train.X*alph_train
         d_train = (((bet_train*e_train)+h_train)/((bet_train**2)+1))
         d_train = np.array(d_train)
         
-#         e_val = df_val.Y - beta0_val - (df_val.X*gam_val)
-#         h_val = alpha0_val + df_val.X*alph_val
-#         d_val = (((bet_val*e_val)+h_val)/((bet_val**2)+1))
-#         d_val = np.array(d_val)
+        output_file_name = algo+'-iter-'+str(i+1)+'-run-'+str(n_runs)+suffix
         
-        early = EarlyStopping(monitor='val_loss', patience=pat,mode='min',verbose=1)
-        mc = ModelCheckpoint(filepath=os.path.join(output_path,'model-iter-'+str(i+1)+'.h5'), verbose=1, monitor='val_loss',save_best_only=True)
-        cb = [early, mc] 
-#         hist = model.fit(X_train,d_train,batch_size=batchSize,validation_data=(X_val,d_val),
-#                                   epochs=nEpochs,verbose=1, shuffle=True,callbacks=cb)
-        hist = model.fit(X_train,d_train,batch_size=batchSize,validation_split=0.3,
+        if algo == 'svr':
+            model = build_model.create_svr_model(X_train,d_train)
+            z_train = model.predict(X_train)
+        else:
+            early = EarlyStopping(monitor='val_loss', patience=pat,mode='min',verbose=1)
+            mc = ModelCheckpoint(filepath=os.path.join(output_path,output_file_name+'.h5'), 
+                             verbose=1, monitor='val_loss',save_best_only=True)
+            cb = [early, mc] 
+            hist = model.fit(X_train,d_train,batch_size=batchSize,validation_split=0.3,
                                   epochs=nEpochs,verbose=1, shuffle=True,callbacks=cb)
-        
-        
-#         hist = parallel_model.fit(X_train,d_train, batch_size, shuffle=True,validation_data=dataGenerator(X_val, d_val, batch_size, meanImg=None, scaling=False,dim=X_train.shape[1:4], shuffle=True, augment=False, maxAngle=40, maxShift=10),epochs=1000,verbose=1,max_queue_size=32,workers=4,use_multiprocessing=False,steps_per_epoch=np.ceil(X_train.shape[0]/batch_size),callbacks=cb)        
-
-
-        z_train = model.predict(X_train)
-#         z_val = model.predict(X_val)
-        
+            z_train = model.predict(X_train)
+            plot_train_loss(hist,os.path.join(output_path,output_file_name+'.png'))
+            
 # Save the params in the pandas dataframe
-        params_df.loc[n_runs]['alpha0','iter_'+str(i)]=alpha0_train
-        params_df.loc[n_runs]['beta0','iter_'+str(i)]=beta0_train
-        params_df.loc[n_runs]['beta','iter_'+str(i)]=bet_train
-        params_df.loc[n_runs]['gamma','iter_'+str(i)]=gam_train
-        params_df.loc[n_runs]['alpha','iter_'+str(i)]=alph_train
-        params_df.to_excel(os.path.join(output_path,'params_'+str(i+1)+'.xlsx'))
+        train_params_df.loc[n_runs]['alpha0','iter_'+str(i)]=alpha0_train
+        train_params_df.loc[n_runs]['beta0','iter_'+str(i)]=beta0_train
+        train_params_df.loc[n_runs]['beta','iter_'+str(i)]=bet_train
+        train_params_df.loc[n_runs]['gamma','iter_'+str(i)]=gam_train
+        train_params_df.loc[n_runs]['alpha','iter_'+str(i)]=alph_train
+        train_params_df.to_excel(os.path.join(output_path,output_file_name+'-train.xlsx'))
         try:
             z_train = np.concatenate(z_train)
-#             z_val = np.concatenate(z_val)
         except:
             pass
-    
-        plot_train_loss(hist,os.path.join(output_path,'model_loss_'+str(i+1)+'.png'))
-        z_val = None
-    return params_df,z_train,z_val,model
+        
+        alpha0_test,alph_test,beta0_test,bet_test,gam_test,z_test = predict_mediation(model,X_test,df_test)
+        test_params_df.loc[n_runs]['alpha0','iter_'+str(i)]=alpha0_train
+        test_params_df.loc[n_runs]['beta0','iter_'+str(i)]=beta0_train
+        test_params_df.loc[n_runs]['beta','iter_'+str(i)]=bet_train
+        test_params_df.loc[n_runs]['gamma','iter_'+str(i)]=gam_train
+        test_params_df.loc[n_runs]['alpha','iter_'+str(i)]=alph_train
+        test_params_df.to_excel(os.path.join(output_path,output_file_name+'-test.xlsx'))
+
+    return train_params_df,test_params_df,z_train
 
 def predict_mediation(model,test_imgs,df_test):
     z_test = model.predict(test_imgs)
-    z_test = np.concatenate(z_test)
+    try:
+        z_test = np.concatenate(z_test)
+    except:
+        pass
     
     if np.corrcoef(z_test,df_test.Y)[0,1] < 0:
           z_test = z_test*(-1)
@@ -264,6 +295,20 @@ def concat_images(image_list,output_fname):
     if output_fname != None:
         all_images.to_filename(output_fname)
     return all_images
+    
+def concat_mnist_images(imga, imgb):
+    """
+    Combines two color image ndarrays side-by-side.
+    """
+    ha,wa = imga.shape[:2]
+    hb,wb = imgb.shape[:2]
+    max_height = np.max([ha, hb])
+    total_width = wa+wb
+    new_img = np.zeros(shape=(max_height, total_width))
+    new_img[:ha,:wa]=imga
+    new_img[:hb,wa:wa+wb]=imgb
+    return new_img
+
 
 def get_gradCAM_map(input_data,model,layer_name,penultimate_layer_name,trial,filter_indices=None,
                     backprop_modifier=None,output_path=None,resample=True,threshold=True,
